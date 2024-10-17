0be39cef62ca04640217bc54c87513d7af8c0c6b
Ines-exe <135113237+Ines-exe@users.noreply.github.com>
2024-10-17 22:59:41 +0300

    Update CowGirl.cs

--- CowGirl.cs
+++ CowGirl.cs
@@ -1,49 +1,134 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class CowGirl : MonoBehaviour
+public class Cowgirl : MonoBehaviour
 {
-    public float moveSpeed =5f; 
+    public float moveSpeed = 5f; 
     private Rigidbody2D rb; 
     private Vector3 ogScale; 
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float m_JumpForce = 400f;
+    [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;
+    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;
+    [SerializeField] private bool m_AirControl = false;
+    [SerializeField] private LayerMask m_WhatIsGround; 
+    [SerializeField] private Transform m_GroundCheck1;
+    [SerializeField] private Transform m_GroundCheck2;
+    [SerializeField] private Transform m_GroundCheck3;
+
+    const float k_GroundedRadius = .2f;
+    private bool m_Grounded; 
+    private bool m_FacingRight = true;  
+    private Vector3 m_Velocity = Vector3.zero;
+
+    [Header("Events")]
+    public UnityEvent OnLandEvent;
+
+    [System.Serializable]
+    public class BoolEvent : UnityEvent<bool> { }
+
+    public BoolEvent OnCrouchEvent;
+    private bool m_wasCrouching = false;
+
+    private void Awake()
     {
+        rb = GetComponent<Rigidbody2D>(); 
+        ogScale = transform.localScale;
 
-     rb = GetComponent<Rigidbody2D>(); 
-     ogScale = transform.localScale;
+        if (OnLandEvent == null)
+            OnLandEvent = new UnityEvent();
 
+        if (OnCrouchEvent == null)
+            OnCrouchEvent = new BoolEvent();
     }
 
-    void FixedUpdate(){
-        //moving left and right
-        float xDirection= Input.GetAxis("Horizontal")*moveSpeed*Time.fixedDeltaTime; 
-        float yDirection= Input.GetAxis("Vertical")*moveSpeed*Time.fixedDeltaTime; 
+    void FixedUpdate()
+    {
+        bool wasGrounded = m_Grounded;
+        m_Grounded = false;
 
-        Vector2 newPos = rb.position + new Vector2(xDirection, yDirection); 
+        Collider2D[] colliders1 = Physics2D.OverlapCircleAll(m_GroundCheck1.position, k_GroundedRadius, m_WhatIsGround);
+        Collider2D[] colliders2 = Physics2D.OverlapCircleAll(m_GroundCheck2.position, k_GroundedRadius, m_WhatIsGround);
+        Collider2D[] colliders3 = Physics2D.OverlapCircleAll(m_GroundCheck3.position, k_GroundedRadius, m_WhatIsGround);
 
-        rb.MovePosition(newPos); 
+        CheckGrounded(colliders1, wasGrounded);
+        CheckGrounded(colliders2, wasGrounded);
+        CheckGrounded(colliders3, wasGrounded);
+
+        float xDirection = Input.GetAxis("Horizontal") * moveSpeed * Time.fixedDeltaTime; 
+        bool crouch = Input.GetKeyDown(KeyCode.S);
+        bool jump = Input.GetKeyDown(KeyCode.W); 
+
+        Move(xDirection, crouch, jump); 
     }
 
-    // Update is called once per frame
-    void Update(){
-    // jumping on top of the obstacles
-        if(Input.GetKeyDown(KeyCode.Space)){
-            rb.AddForce(Vector2.up*500);  
+    void CheckGrounded(Collider2D[] colliders, bool wasGrounded)
+    {
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                m_Grounded = true;
+                if (!wasGrounded)
+                    OnLandEvent.Invoke();
+                break;
+            }
         }
     }
 
-    private void OnMouseDown(){
-    //ducking away from obstacles on top
-        transform.localScale = new Vector3(ogScale.x,ogScale.y*0.5f,ogScale.z);
+    public void Move(float move, bool crouch, bool jump)
+    {
+        if (m_Grounded || m_AirControl)
+        {
+            // Handle crouching
+            if (crouch && !m_wasCrouching)
+            {
+                m_wasCrouching = true;
+                OnCrouchEvent.Invoke(true);
+                StartCoroutine(CrouchForSeconds(2f)); // Start coroutine to crouch for 2 seconds
+            }
 
-        StartCoroutine(Standup());
+            if (m_wasCrouching)
+            {
+                move *= m_CrouchSpeed; // Reduce speed when crouching
+            }
+
+            // Move character
+            Vector3 targetVelocity = new Vector2(move * 10f, rb.velocity.y);
+            rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+            // Flip character if necessary
+            if (move > 0 && !m_FacingRight)
+            {
+                Flip();
+            }
+            else if (move < 0 && m_FacingRight)
+            {
+                Flip();
+            }
+        }
+
+        // Jump
+        if (m_Grounded && jump)
+        {
+            m_Grounded = false;
+            rb.AddForce(new Vector2(0f, m_JumpForce));
+        }
     }
 
-    private IEnumerator Standup(){
-        yield return new WaitForSeconds(0.1f); 
-        transform.localScale = ogScale; 
+    private IEnumerator CrouchForSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        m_wasCrouching = false; // Reset crouch state
+        OnCrouchEvent.Invoke(false); // Notify that the player has stood up
+    }
+
+    void Flip()
+    {
+        m_FacingRight = !m_FacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
